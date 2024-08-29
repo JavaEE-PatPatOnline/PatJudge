636b88517fdd198773ed6d2d30cc68b49ce8aa4c
Lord Turmoil <tony-turmoil@outlook.com>
2024-08-29 20:00:37 +0800

    update: add copyright info

--- src/main/java/cn/edu/buaa/patpat/judge/services/ICompiler.java
+++ src/main/java/cn/edu/buaa/patpat/judge/services/ICompiler.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) Patpat Online 2024
+ * Made with love by Tony Skywalker
+ */
+
 package cn.edu.buaa.patpat.judge.services;
 
 import cn.edu.buaa.patpat.judge.extensions.judge.JudgeErrorException;
